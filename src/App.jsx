fdb4f7af444f68dd773544eb93fa922699eca655
zaidsmd <zaidsmd111@gmail.com>
2024-02-23 23:26:59 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './styles/scss/main.scss'
 import Navbar from "./components/Navbar";
+import {HeroSection} from "./components/HeroSection";
 
 function App() {
 
   return (
     <>
         <Navbar></Navbar>
+        <HeroSection></HeroSection>
     </>
   )
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+import '../styles/scss/hero.scss'
+export function HeroSection() {
+    return (
+        <>
+            <div id="hero">
+
+            </div>
+        </>
+    )
+}
